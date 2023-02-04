654f88c18db2f367732b065e50f2033c86692d7d
Pablo Correa <juanpabloc2000@gmail.com>
2023-02-04 09:44:02 -0500

    Movement Remake alpha

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,48 +6,65 @@ public class Movement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
+    public Transform target;
     public float speed,
         jumpSpeed,
         turnSmoothTime,
         turnSmoothVelocity;
     private float gravity = -20f;
     private Vector3 moveVel;
+    private Vector3 playerInput;
+    private bool jumping;
+    private Vector3 camRight;
+    private Vector3 camForward;
 
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        playerInput = new Vector3(horizontal, 0f, vertical);
 
-        if (direction.magnitude >= 0.1f && controller.isGrounded)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        //if (controller.isGrounded)
+        //{
+            playerInput = Vector3.ClampMagnitude(playerInput, 1f);
+
+            camDir();
+
+
+            moveVel = playerInput.x * camRight + playerInput.z * camForward;
+        target.transform.LookAt(target.transform.position + moveVel);
+        transform.rotation = Quaternion.Lerp(transform.rotation, target.rotation, 5 * Time.deltaTime);
+            //controller.transform.LookAt(controller.transform.position + moveVel);
+            jumping = false;
+            /*float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;*/
+            //moveVel = moveDir;
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             
 
 
-            moveVel = moveDir;
 
-        }
-        else if (!controller.isGrounded)
+        //}
+        /*else if (!controller.isGrounded && jumping)
         {
-            if (horizontal >= 0.1)
-                moveVel.x = cam.right.x * speed;
-            else if (horizontal <= -0.1)
-                moveVel.x = cam.right.x * -speed;
-            if (vertical >= 0.1)
-                moveVel.z = cam.forward.z * speed;
-            else if (vertical <= -0.1)
-                moveVel.z = cam.forward.z * -speed;
+            float n = 0.4f;
+            if (horizontal >= n)
+                moveVel.x = target.right.x * speed;
+            else if (horizontal <= -n)
+                moveVel.x = target.right.x * -speed;
+            if (vertical >= n)
+                moveVel.z = target.forward.z * speed;
+            else if (vertical <= -n)
+                moveVel.z = target.forward.z * -speed;
         }
 
         if (Input.GetButtonDown("Jump") && controller.isGrounded)
         {
             moveVel.y = jumpSpeed;
-        }
+            jumping = true;
+        }*/
 
         if (moveVel.z >= 0.1)
         {
@@ -56,7 +73,19 @@ public class Movement : MonoBehaviour
         /*else
             GetComponent<PlayerController>().anim.SetBool("Running", false);*/
 
-        moveVel.y += gravity * Time.deltaTime;
-        controller.Move(moveVel.normalized * speed * Time.deltaTime);
+        //moveVel.y += gravity * Time.deltaTime;
+        controller.Move(moveVel * speed * Time.deltaTime);
+    }
+
+    void camDir()
+    {
+        camForward = Camera.main.transform.forward;
+        camRight = Camera.main.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
     }
 }
