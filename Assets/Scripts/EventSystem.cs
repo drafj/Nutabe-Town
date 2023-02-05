75d223fc6b852b57b5253be398f340d0872b996a
EDWARD-GONZALEZ <jhone.gonzalez@udea.edu.co>
2023-02-05 10:19:33 -0500

    Canvas
    Implementacion del canvas

---
+++ Assets/Scripts/EventSystem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EventSystem : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Credits()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Exit()
+    {
+        Exit();
+    }
+}
