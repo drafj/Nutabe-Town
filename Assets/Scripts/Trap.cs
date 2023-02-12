34cc52f6c6bc03835438f85cb7de98fd4d7c2ba3
drafj <juanpabloc2000@gmail.com>
2023-02-12 15:16:43 -0500

    trap script started

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public List<Transform> trapPositions = new List<Transform>();
+    public int trapPosIndex = 0;
+}
